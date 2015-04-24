741dfb9c0f55e75b31cdd2141a2b77e0a5e67820
MiGu3X <miguefutbolboca@gmail.com>
2015-04-23 22:18:41 -0300

    agrego tp1

---
+++ TP_1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCuenta = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCuenta
+            // 
+            this.txtCuenta.Location = new System.Drawing.Point(97, 23);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(268, 20);
+            this.txtCuenta.TabIndex = 0;
+            this.txtCuenta.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtCuenta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCuenta_KeyPress_1);
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(42, 86);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(72, 57);
+            this.btnSuma.TabIndex = 1;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click_1);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(199, 86);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(72, 57);
+            this.btnResta.TabIndex = 2;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click_1);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(347, 86);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(72, 57);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "AC";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion.Location = new System.Drawing.Point(42, 215);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(72, 57);
+            this.btnMultiplicacion.TabIndex = 4;
+            this.btnMultiplicacion.Text = "*";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click_1);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.Location = new System.Drawing.Point(199, 215);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(72, 57);
+            this.btnDivision.TabIndex = 5;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click_1);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.Location = new System.Drawing.Point(347, 215);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(72, 57);
+            this.btnIgual.TabIndex = 6;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 345);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtCuenta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnIgual;
+    }
+}
+
---
+++ TP_1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        float num1, num2, resultado;
+        char operacion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void txtCuenta_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= (char)Keys.D0 && e.KeyChar <= (char)Keys.D9) || (byte)e.KeyChar == 8 || e.KeyChar == ',' || e.KeyChar == '-' || (byte)e.KeyChar == 13)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            if ((byte)e.KeyChar == 13) // Enter
+            {
+                btnIgual.PerformClick();
+            }
+            else
+            {
+                if ((byte)e.KeyChar == 43) // El más
+                {
+                    btnSuma.PerformClick();
+                }
+                else
+                {
+                    if ((byte)e.KeyChar == 45) // El menos
+                    {
+                        btnResta.PerformClick();
+                    }
+                    else 
+                    {
+                        if ((byte)e.KeyChar == 47) // El dividido
+                        {
+                            btnDivision.PerformClick();
+                        }
+                        else
+                        {
+                            if ((byte)e.KeyChar == 42) // El multiplicador
+                            {
+                                btnMultiplicacion.PerformClick();
+                            }
+                        }
+                    }
+                }
+            }
+            btnSuma.Enabled = false;
+            btnResta.Enabled = false;
+            btnMultiplicacion.Enabled = false;
+            btnDivision.Enabled = false;
+        }
+        private void btnSuma_Click_1(object sender, EventArgs e)
+        {
+            if (txtCuenta.Text != "")
+            {
+                num1 = Convert.ToSingle(txtCuenta.Text);
+                txtCuenta.Clear();
+                txtCuenta.Focus();
+                operacion = '+';
+            }
+            else
+            {
+                MessageBox.Show("Math ERROR");
+            }
+        }
+        private void btnResta_Click_1(object sender, EventArgs e)
+        {
+            if (txtCuenta.Text != "")
+            {
+                num1 = Convert.ToSingle(txtCuenta.Text);
+                txtCuenta.Clear();
+                txtCuenta.Focus();
+                operacion = '-';
+            }
+            else
+            {
+                MessageBox.Show("Math ERROR");
+            }
+        }
+        private void btnMultiplicacion_Click_1(object sender, EventArgs e)
+        {
+            if (txtCuenta.Text != "")
+            {
+                num1 = Convert.ToSingle(txtCuenta.Text);
+                txtCuenta.Clear();
+                txtCuenta.Focus();
+                operacion = '*';
+            }
+            else
+            {
+                MessageBox.Show("Math ERROR");
+            }
+        }
+        private void btnDivision_Click_1(object sender, EventArgs e)
+        {
+            if (txtCuenta.Text != "")
+            {
+                num1 = Convert.ToSingle(txtCuenta.Text);
+                txtCuenta.Clear();
+                txtCuenta.Focus();
+                operacion = '/';
+            }
+            else
+            {
+                MessageBox.Show("Math ERROR");
+            }
+        }
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            txtCuenta.Clear();
+            txtCuenta.Focus();
+            btnSuma.Enabled = true;
+            btnResta.Enabled = true;
+            btnMultiplicacion.Enabled = true;
+            btnDivision.Enabled = true;
+        }
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            if(txtCuenta.Text != "")
+            {
+                if (operacion == '+')
+                {
+                    num2 = Convert.ToSingle(txtCuenta.Text);
+                    resultado = num1 + num2;
+                    txtCuenta.Text = Convert.ToString(resultado);
+                }
+                else if (operacion == '-')
+                {
+                    num2 = Convert.ToSingle(txtCuenta.Text);
+                    resultado = num1 - num2;
+                    txtCuenta.Text = Convert.ToString(resultado);
+                }
+                else if (operacion == '*')
+                {
+                    num2 = Convert.ToSingle(txtCuenta.Text);
+                    resultado = num1 * num2;
+                    txtCuenta.Text = Convert.ToString(resultado);
+                }
+                else if (operacion == '/')
+                {
+                    num2 = Convert.ToSingle(txtCuenta.Text);
+                    if (num2 > 0)
+                    {
+                        resultado = num1 / num2;
+                        txtCuenta.Text = Convert.ToString(resultado);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Math ERROR");
+                    }
+                }
+                btnSuma.Enabled = true;
+                btnResta.Enabled = true;
+                btnMultiplicacion.Enabled = true;
+                btnDivision.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Math ERROR");
+            }
+        }
+    }
+}
